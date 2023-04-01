ac00ab8e5f10ac0ca74712b746ea2de9da991e52
Rachapon23 <ohm32070@gmail.com>
2023-04-01 08:39:56 +0700

    upload carousel example

---
+++ example/CarouselExample.js.js
@@ -0,0 +1,40 @@
+import { Carousel, Col, Row } from 'antd';
+
+const contentStyle = {
+  height: '300px',
+  color: '#fff',
+  lineHeight: '300px',
+  textAlign: 'center',
+  background: '#364d79',
+};
+
+function App() {
+
+  const carouselCard = (data) => {
+    return (
+      <div>
+        <h3 style={contentStyle}>{data}</h3>
+      </div>
+    )
+  }
+
+  const listCarouselCard = [carouselCard("1 - Test"), carouselCard("2 - Example"), carouselCard("3 - Setting"), carouselCard("4 - Start")]
+
+  return (
+    <div>
+      <Row justify={"center"} align={"middle"} style={{paddingTop: "20%"}}>
+        <Col style={{width: "80%"}}>
+          <Carousel autoplay autoplaySpeed={5000}>
+            {
+              listCarouselCard.map((card, index) => (
+                <Col key={index}>{card}</Col>
+              ))
+            }
+          </Carousel>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default App;
